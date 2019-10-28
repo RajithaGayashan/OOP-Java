40c47c01b2b5fd4ace27446c83dffa3bd6fa4889
RajithaGayashan <rajithagayashan95@gmail.com>
2019-10-28 15:54:50 +0530

    Java oop first

---
+++ Test1.java
@@ -0,0 +1,11 @@
+class Test1{
+    public static void main(String args[]){
+        Newclass t=new Newclass();
+        t.m();
+    }
+}
+class Newclass{
+    public void m(){
+        System.out.println("Hello world");
+    }
+}
