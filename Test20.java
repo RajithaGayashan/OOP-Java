14e2af0727c86a3877c00bf4d0f472178ea6ce57
RajithaGayashan <rajithagayashan95@gmail.com>
2020-03-23 20:49:14 +0530

    java example

---
+++ Test20.java
@@ -0,0 +1,9 @@
+class Test52{
+    public static void main(String args[]){
+        System.out.println("Index number\tMarks");
+        int Marks[]={12,66,92,44,66};
+        for(int i=0;i<=Marks.length;i++){
+            System.out.println((i+1)+"\t\t"+Marks[i]);
+        }
+    }
+}
