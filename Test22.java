f9a7b119e1ec66cdc2d22d96fb79a4e45fbfc893
RajithaGayashan <rajithagayashan95@gmail.com>
2020-03-26 23:50:57 +0530

    continue statement

---
+++ Test22.java
@@ -0,0 +1,12 @@
+class Test44{
+    public static void main(String args[]){
+        stp :for(int j=1;j<10;j++){
+            for(int i=1;i<5;i++){
+                System.out.println(j+"   "+i);
+                if(i==2)
+                continue stp ;
+        
+            }
+        }
+    }
+}
