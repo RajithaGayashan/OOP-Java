4c4f428ab4a16c70a3e7b3d05e4ca252a79f3ca2
RajithaGayashan <rajithagayashan95@gmail.com>
2020-04-06 20:50:07 +0530

    Switch case

---
+++ Test26.java
@@ -0,0 +1,16 @@
+class Test21{
+    public static void main(String args[]){
+        int a=1;
+        switch(a=4){
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            default:
+                System.out.println("Default");
+                break;
+        }
+    }
+}
