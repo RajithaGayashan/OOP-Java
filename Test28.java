2d0b192af919e7b23820462c8aecb924c298b348
RajithaGayashan <rajithagayashan95@gmail.com>
2020-04-14 10:50:58 +0530

    do while loop

---
+++ Test28.java
@@ -0,0 +1,18 @@
+class Test16{
+    public static void main(String[] args){
+        int y=0;
+        
+        do{
+            int x=0;    
+            do{
+            System.out.print("*");
+            x++;
+        }while(x<10);
+        
+            System.out.println();
+            y++;        
+        }
+ 
+        while(y<10);
+    }
+}
