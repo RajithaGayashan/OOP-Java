d7256a4c45359b47947ea1c2da9c07f77debd214
RajithaGayashan <rajithagayashan95@gmail.com>
2020-08-04 10:09:48 +0530

    Constructors

---
+++ Test30.java
@@ -0,0 +1,11 @@
+public class Test30 {
+  int x;  
+
+  public Test30() {
+    x = 5;  
+  }
+ public static void main(String[] args) {
+	Test30 myObj = new Test30(); 
+    System.out.println(myObj.x); 
+  }
+}
