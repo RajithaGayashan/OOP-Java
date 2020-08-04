8262aec7bc48087aa75d85f11126df30c18a88a7
RajithaGayashan <rajithagayashan95@gmail.com>
2020-08-04 10:16:20 +0530

    Constructor Parameters

---
+++ Test31.java
@@ -0,0 +1,12 @@
+public class Test31 {
+  int x;
+
+  public Test31(int y) {
+    x = y;
+  }
+
+  public static void main(String[] args) {
+    Test31 myObj = new Test31(10);
+    System.out.println(myObj.x);
+  }
+}
