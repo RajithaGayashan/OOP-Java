23d229a41b75b68eb83ec06daf6606cf2280cda9
RajithaGayashan <rajithagayashan95@gmail.com>
2020-08-05 02:10:38 +0530

    Java Abstract Classes and Methods

---
+++ Test36.java
@@ -0,0 +1,20 @@
+abstract class Animal {
+  public abstract void animalSound();
+  public void sleep() {
+    System.out.println("Zzz");
+  }
+}
+
+class Pig extends Animal {
+  public void animalSound() {
+    System.out.println("The pig says: wee wee");
+  }
+}
+
+class Test36 {
+  public static void main(String[] args) {
+    Pig myPig = new Pig(); 
+    myPig.animalSound();
+    myPig.sleep();
+  }
+}
