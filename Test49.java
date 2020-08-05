0f879c9d1c85305b3bbdd4e4697cdcb955846ae0
RajithaGayashan <rajithagayashan95@gmail.com>
2020-08-06 02:02:13 +0530

    Thread - Concurrency Problems

---
+++ Test49.java
@@ -0,0 +1,15 @@
+public class Test49 extends Thread {
+  public static int amount = 0;
+
+  public static void main(String[] args) {
+    Test49 thread = new Test49();
+    thread.start();
+    System.out.println(amount);
+    amount++;
+    System.out.println(amount);
+  }
+
+  public void run() {
+    amount++;
+  }
+}
