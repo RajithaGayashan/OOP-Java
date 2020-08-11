500de9fba7c45ea0681f5c5c27da43c0b49e83de
RajithaGayashan <rajithagayashan95@gmail.com>
2020-08-11 22:42:41 +0530

    swap two numbers in java with using third variable

---
+++ Test54.java
@@ -0,0 +1,10 @@
+public class Test54 {
+    public static void main(String args[]){
+        int x=10;
+        int y=5;
+        x=x+y;
+        y=x-y;
+        x=x-y;
+        System.out.println("After swap : X = "+x+" , y = "+y);
+    }
+}
