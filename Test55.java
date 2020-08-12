a1bb1917df81c9b897cc3e46b319baf0dfd1b2bd
RajithaGayashan <rajithagayashan95@gmail.com>
2020-08-12 15:34:15 +0530

    Autoboxing

---
+++ Test55.java
@@ -0,0 +1,10 @@
+class Test55{  
+  public static void main(String args[]){  
+    int a=50;  
+        Integer a2=new Integer(a);//AutoBoxing  
+  
+        Integer a3=5;//AutoBoxing  
+          
+        System.out.println(a2+" "+a3);  
+ }   
+}
